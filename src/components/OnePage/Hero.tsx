d1cc961d186b71aa9720448a12d2cd71ab0fd98b
Juan Miguel <juanmiguelriveroyhornos@gmail.com>
2025-12-24 10:59:11 -0300

    Change

--- src/components/OnePage/Hero.tsx
+++ src/components/OnePage/Hero.tsx
@@ -15,15 +15,13 @@ export default function Hero() {
                 </div>
 
                 <h1 className="text-2xl font-black uppercase tracking-tighter text-white sm:text-7xl md:text-3xl leading-[0.9]">
-                    Más que un sitio web: <br />
-                    <span className="block text-white">El centro de tu</span>
-                    <span className="block text-neon-lime">Universo Digital.</span>
+                    Headless Architect (Next.js + WPGraphQL)<br />
+                    <span className="block text-white">Building High-Performance Web Experiences. </span>
+
                 </h1>
 
                 <p className="mx-auto max-w-3xl text-md font-medium text-gray-400 sm:text-xl leading-relaxed">
-                    En el cosmos digital, una <span className="text-neon-lime">página web</span> no es una tarjeta de presentación.
-                    Es tu base de operaciones, tu fortaleza y el <span className="text-neon-lime">motor de tu crecimiento</span>,
-                    sobre cimientos de <span className="text-neon-lime">soberanía</span>, <span className="text-neon-lime">velocidad</span> y <span className="text-neon-lime">poder</span>.
+                    Development of a high-performance web platform by decoupling the frontend from the backend to maximize Core Web Vitals and security.
                 </p>
 
                 <div className="pt-8">
