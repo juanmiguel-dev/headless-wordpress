68debab1afce1495834f67c5455ed86a1963c416
Juan Miguel <juanmiguelriveroyhornos@gmail.com>
2025-12-25 14:09:17 -0300

    Change

--- src/app/concept/page.tsx
+++ src/app/concept/page.tsx
@@ -75,7 +75,7 @@ export default function ConceptPage() {
                     {/* Visual Card */}
                     <div className="md:col-span-7 relative group overflow-hidden rounded-sm h-[400px] md:h-[600px] border border-white/5">
                         <Image
-                            src="https://v2.pachadev.com/wp-content/uploads/2025/12/pikaso_texttoimage_lettering-de-DESARROLLO-WEB-con-fondo-negro-El-bac.jpeg?q=80&w=2070&auto=format&fit=crop"
+                            src="https://v2.pachadev.com/wp-content/uploads/2025/12/sunset-8331285_1920.jpg?q=80&w=2070&auto=format&fit=crop"
                             alt="Performance architecture"
                             fill
                             className="object-cover transition-transform duration-1000 group-hover:scale-110 opacity-60 group-hover:opacity-100"
